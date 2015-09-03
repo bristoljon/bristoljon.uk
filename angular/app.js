d65a72e3724135b48617ba0ee4c4fb4779c12af4
bristoljon <jon@bristoljon.uk>
2015-09-03 09:56:07 +0100

    Added numberFormatter function to Units, sortBy filters, Removed lifeHours

--- angular/app.js
+++ angular/app.js
@@ -1,33 +1,24 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope','$filter','$log','$timeout', function($scope,$filter,$log,$timeout) {
+myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
 
-    $scope.handle="";
+    $scope.handle = '';
 
-    $scope.lowercase = function () {
-    	return $filter('lowercase')($scope.handle);
+    $scope.lowercasehandle = function () {
+        return $filter('lowercase')($scope.handle);
     };
 
     $scope.characters = 5;
-    
-    var updatesrequest = new XMLHttpRequest();
-    var url = "/updates.php";
 
-	updatesrequest.open("POST", url, true);
-	updatesrequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	
-    updatesrequest.onreadystatechange = function () {
-    	$scope.$apply(function() {
-
-	    	if (updatesrequest.readyState == 4 && updatesrequest.status ==200) {
-	    		$scope.updates = JSON.parse(updatesrequest.responseText);
-	    	}
-	    	else $log.log("Failed");
-
-	    	$log.log($scope.updates);
-	    })
+    var rulesrequest = new XMLHttpRequest();
+    rulesrequest.onreadystatechange = function () {
+        $scope.$apply(function () {
+            if (rulesrequest.readyState == 4 && rulesrequest.status == 200) {
+                $scope.rules = JSON.parse(rulesrequest.responseText);
+            }
+        });
     }
+    rulesrequest.open("GET", "http://localhost:54765/api", true);
+    rulesrequest.send();
 
-	updatesrequest.send("get=recent");
-    
 }]);
--- units/app.js
+++ units/app.js
@@ -1,39 +1,84 @@
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    console.log(local.toJSON().slice(0,16));
+    return local.toJSON().slice(0,16);
+});
+
+// Add method to remove object from array based on unit title
+Array.prototype.remove = function(title) {
+    for (i=0; i<this.length; i++) {
+      if (this[i].title === title) this.splice(i);
+    }
+    return this;
+};
+
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope','$filter','$log','$timeout', function($scope,$filter,$log,$timeout) {
 
+  // Needed to prevent 'Life Hours' being calculated and entered into array more than once
+	$scope.percentWorking =0;
+	var initialised=0;
+
+	// Set up array of money unit objects
     function moneyUnit (title,pounds) {
     	this.title = title;
     	this.pounds = pounds;
     }
 
-    var quid = new moneyUnit ("£ GBP",1);
-    var ps4 = new moneyUnit ("Playstation 4",300);
+    $scope.moneyUnits =[];
+    $scope.moneyUnits.push(new moneyUnit("£ GBP",1));
+    $scope.moneyUnits.push(new moneyUnit("Jerk Island Lunch Special",3));
+    $scope.moneyUnits.push(new moneyUnit("Playstation 4",300));
+    $scope.moneyUnits.push(new moneyUnit("London Studio Flat",300000));
 
-    $scope.moneyUnits = [quid,ps4];
 
+    // Set up array of time unit objects
     function timeUnit (title,hours) {
     	this.title = title;
     	this.hours = hours;
     }
 
-    var hour = new timeUnit ("Hours",1);
-    var day = new timeUnit ("Days",24);
-    var week = new timeUnit ("Weeks",24*7);
-    var month = new timeUnit ("Months",24*30);
-    var year = new timeUnit ("Years",24*365);
+    $scope.timeUnits =[];
+    $scope.timeUnits.push(new timeUnit("Hours",1));
+    $scope.timeUnits.push(new timeUnit("Days",24));
+    $scope.timeUnits.push(new timeUnit("Weeks",24*7));
+    $scope.timeUnits.push(new timeUnit("Months",24*30));
+    $scope.timeUnits.push(new timeUnit("Years",24*365));
+    $scope.timeUnits.push(new timeUnit("Wank (average male)",0.4));
+    $scope.timeUnits.push(new timeUnit("London - Bristol Coach Trip",3));
+    $scope.timeUnits.push(new timeUnit("Atlantic Crossing by Pedalo",2664));
 
-    var wank = new timeUnit ("Tommy Tank (average male)",0.4);
-    var coach = new timeUnit ("Bristol - London Coach",3);
-
-    $scope.timeUnits = [hour,day,week,month,year,wank,coach];
 
     $scope.customMoneyAdd = function() {
     	$scope.moneyUnits.push(new moneyUnit($scope.customMoneyTitle,$scope.customMoneyPounds));
     }
 
-    $scope.removeUnit = function() {
-    	$log.log("clicked");
+    $scope.customTimeAdd = function() {
+    	$scope.timeUnits.push(new timeUnit($scope.customMoneyTitle,$scope.customMoneyPounds));
+    }
+
+    $scope.removeUnit = function(unit) {
+    	$scope.moneyUnits.remove(unit);
+    }
+
+    $scope.sortTimeBy = function (what) {
+    	if (what === "Hours") {
+    		$scope.timeUnits.sort(function(a,b) {return a.hours - b.hours});
+    	}
+    	else {
+    		$scope.timeUnits.sort(function(a,b) {return a.title > b.title});
+    	}
+    }
+
+    $scope.sortMoneyBy = function (what) {
+    	if (what === "Pounds") {
+    		$scope.moneyUnits.sort(function(a,b) {return a.pounds - b.pounds});
+    	}
+    	else {
+    		$scope.moneyUnits.sort(function(a,b) {return a.title > b.title});
+    	}
     }
 
     $scope.calculatePay = function() {
@@ -46,20 +91,25 @@ myApp.controller('mainController', ['$scope','$filter','$log','$timeout', functi
 
 	    // Convert hourly to true hourly rate of pay
 	    $scope.truePayPerHour = $scope.payPerHour/$scope.workPerHour;
-    	
+
+	    // If we have all information calculate 'Life Hour' units and push to array
+	    /*
+	    if ($scope.percentWorking != 0 && initialised==0) {
+		    $scope.timeUnits.push(new timeUnit("Life Hours",1*($scope.percentWorking/100)));
+		    $scope.timeUnits.push(new timeUnit("Life Days",24*($scope.percentWorking/100)));
+		    $scope.timeUnits.push(new timeUnit("Life Weeks",24*7*($scope.percentWorking/100)));
+		    $scope.timeUnits.push(new timeUnit("Life Months",24*30*($scope.percentWorking/100)));
+		    $scope.timeUnits.push(new timeUnit("Life Years",24*365*($scope.percentWorking/100)));
+    		initialised =1;
+    	}
+    	*/
     }
 
-    $scope.calculateHours = function() {
-    	
-    	// Convert weekly to daily then hourly
+    $scope.calculateHours = function() {   	
 	    $scope.workPerHour = ($scope.hours/7)/24;
-
-	    $scope.workPerDay = $scope.workPerHour * 24;
-    	
+	    $scope.workPerDay = $scope.workPerHour * 24;    	
     	$scope.percentWorking = ($scope.workPerHour/1)*100;
-
     	$scope.yearsRemaining = 79.2-$scope.age;
-
     	$scope.workYearsRemaining = (65-$scope.age)*($scope.percentWorking/100);
   	}
 
@@ -115,10 +165,83 @@ myApp.controller('mainController', ['$scope','$filter','$log','$timeout', functi
   				outVal = (($scope.inVal * inPounds) / $scope.truePayPerHour)*outHours;
   			}
   		}
-  		console.log(outVal);
-  		$scope.outVal = outVal.toFixed(2);
+  		$scope.outVal = numberFormatter(outVal);
+  	}
 
-  		
+
+  	function numberFormatter(inVal) {
+
+  		function toSignificantFigures(inVal,sf) {
+
+ 			var fracPart, chopPoint=0;
+  			inVal = inVal.toString();
+
+  			for (i=0; i<inVal.length; i++) {
+  				if (fracPart === 1 && chopPoint === 0) {
+  					if (inVal.charAt(i) != 0) {
+  						chopPoint = i+sf;
+  						break;
+  					}
+  				}
+  				else if (inVal.charAt(i) === ".") fracPart = 1;	
+  			}
+  			return inVal.substr(0,chopPoint);
+  		}
+
+  		var test, outVal;
+
+  		// If NaN i.e no out unit defined
+  		if (isNaN(parseFloat(inVal))) {
+  			outVal = 0;
+  		}
+
+  		// If zero display zero
+  		else if (inVal === 0) {
+  			outVal = 0;
+  		}
+
+  		// If greater than one..
+  		else if (inVal>=1) {
+  			test = inVal % inVal.toFixed(0);
+          	if (test === 0) {
+            	outVal = inVal.toFixed(0);
+          	}
+			else {
+				outVal = toSignificantFigures(inVal,2);
+			}	
+		}
+
+  		else {
+	  		// Less than 1 display as percentage
+	  		inVal *= 100;
+			test = inVal % inVal.toFixed(0);
+			if (test === 0) {
+				$log.log("Round number percentage < 1 ");
+				outVal = inVal.toFixed(0)+"%";
+			}
+			else {
+				$log.log("Fracional percentage < 1");
+				outVal = toSignificantFigures(inVal,2)+"%";
+			}
+  		}
+  		return outVal;
   	}
     
+
+    function counterUpper (type,name,start,unit) {
+    	this.name = name;
+    	this.type = type;
+    	this.startTime = start || Date.now();
+    	this.unit = unit;
+    	this.elapsedSeconds = 0;
+    	this.elapsedUnits = 0;
+
+    	this.secondsSinceStart = function () {
+    		this.elapsedSeconds = Date.now() - this.startTime;
+  		}
+  		this.unitsSinceStart = function () {
+  			
+  		}
+    }
+
 }]);
